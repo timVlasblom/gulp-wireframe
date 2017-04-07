43cad5ee4a0e9115b64f56b84d7bbfdaf51b6515
Tim Vlasblom <Tim Vlasblom>
2017-04-07 10:17:34 +0200

    BEMified all the things

--- gulp/gulp-minify.js
+++ gulp/gulp-minify.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var cleanCSS = require('gulp-clean-css');
 
-gulp.task('minify-css', function() {
+gulp.task('minify-css', ['sass'], function() {
   return gulp.src('build/style.css')
     .pipe(cleanCSS({compatibility: 'ie8'}))
     .pipe(gulp.dest('build/min'));
